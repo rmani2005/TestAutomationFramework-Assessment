b602b001f112291f49234807e15b6897d14ab78c
manikandanravi <rmani2005@gmail.com>
2023-10-30 14:20:57 -0400

    modified the chromeheadlessstrategy file with some argument

--- src/main/java/com/clover/solution/ui/browser/chromeHeadlessDriverStrategy.java
+++ src/main/java/com/clover/solution/ui/browser/chromeHeadlessDriverStrategy.java
@@ -14,6 +14,7 @@ public class chromeHeadlessDriverStrategy extends browserDriverStrategy
     {
     	
             options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
             options.addArguments("--headless=new");
             options.addArguments("--headless");
             options.addArguments("start-maximized"); // open Browser in maximized mode
@@ -21,12 +22,11 @@ public class chromeHeadlessDriverStrategy extends browserDriverStrategy
             options.addArguments("--disable-extensions"); // disabling extensions
             options.addArguments("--disable-gpu"); // applicable to windows os only
             options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            options.addArguments("--no-sandbox"); // Bypass OS security model
+             // Bypass OS security model
             //options.addArguments("window-size=1920,1080");
             options.addArguments("--log-level=3","--remote-allow-origins=*");
             //options.setAcceptInsecureCerts(true);
             //System.setProperty("webdriver.chrome.silentOutput", "true");
-            
             //options.addArguments(Arrays.asList("--window-position=0,0"));
            // options.addArguments(Arrays.asList("--window-size=1920,1080"));
 
